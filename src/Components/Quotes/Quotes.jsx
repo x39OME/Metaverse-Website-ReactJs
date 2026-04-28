e96d4acf147d967ee5725ad526b0845b82f537f7
unknown <x39OME@outlook.sa>
2026-04-28 18:48:42 +0300

    initial commit

---
+++ src/Components/Quotes/Quotes.jsx
@@ -0,0 +1,17 @@
+
+const Quotes = () => {
+  return (
+    <>
+      <div className="container py-14 px-4">
+        <h1 data-aos="zoom-in" className="text-2xl sm:text-4xl font-bold max-w-[650px] mx-auto text-center font-parisienne">
+          <q>
+            Virtual reality is a medium, a means by which humans can share ideas
+            and experiences
+          </q>
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default Quotes;
